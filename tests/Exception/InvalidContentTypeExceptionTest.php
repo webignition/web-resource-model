4d8a94afeeeba50a454ad8cc9fa555afcabf28d0
Jon Cram <webignition@gmail.com>
2018-10-02 21:34:11 +0100

    Re-add InvalidContentTypeException (#17)

---
+++ tests/Exception/InvalidContentTypeExceptionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace webignition\Tests\WebResource\Exception;
+
+use Mockery\Mock;
+use Psr\Http\Message\ResponseInterface;
+use webignition\InternetMediaTypeInterface\InternetMediaTypeInterface;
+use webignition\WebResource\Exception\InvalidContentTypeException;
+
+class InvalidContentTypeExceptionTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider getContentTypeDataProvider
+     *
+     * @param InternetMediaTypeInterface $responseContentType
+     * @param string $expectedContentTypeString
+     */
+    public function testGetContentType($responseContentType, $expectedContentTypeString)
+    {
+        $response = \Mockery::mock(ResponseInterface::class);
+        $response
+            ->shouldReceive('getReasonPhrase')
+            ->andReturn('OK');
+        $response
+            ->shouldReceive('getStatusCode')
+            ->andReturn(200);
+
+        $exception = new InvalidContentTypeException($responseContentType);
+
+        $this->assertEquals($expectedContentTypeString, (string)$exception->getContentType());
+    }
+
+    /**
+     * @return array
+     */
+    public function getContentTypeDataProvider()
+    {
+        return [
+            'default' => [
+                'responseContentType' => $this->createContentType('text/plain'),
+                'expectedContentTypeString' => 'text/plain',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $typeSubtypeString
+     *
+     * @return InternetMediaTypeInterface|Mock
+     */
+    private function createContentType($typeSubtypeString)
+    {
+        /* @var InternetMediaTypeInterface|Mock $contentType */
+        $contentType = \Mockery::mock(InternetMediaTypeInterface::class);
+
+        $contentType
+            ->shouldReceive('getTypeSubtypeString')
+            ->andReturn($typeSubtypeString);
+
+        $contentType
+            ->shouldReceive('__toString')
+            ->andReturn($typeSubtypeString);
+
+        return $contentType;
+    }
+}
